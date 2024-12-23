a53df85dc2be1556e31aab39e80e11c4e29957df
Saehan <jungsae@oregonstate.edu>
2024-12-23 15:37:31 +0900

    [FIX] axios

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import { routes } from 'vue-router/auto-routes';
 import { useAuthStore } from '@/stores/auth';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
   routes: setupLayouts(routes),
 });
 
