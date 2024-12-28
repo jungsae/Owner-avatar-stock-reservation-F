0ab66064788dfba2c9a0d70639bf1f3fbd980cd6
jungsae <wjdtpgks388@gmail.com>
2024-12-29 03:32:43 +0900

    [FIX] 9.3

--- src/utils/imageLoader.js
+++ src/utils/imageLoader.js
@@ -1,4 +1,6 @@
 export const getImageUrl = (imageUrl) => {
-    console.log(imageUrl)
-    return new URL(`../assets/cakes/${imageUrl}.png`, import.meta.url).href;
+    const fileName = imageUrl.split('/').pop();
+    const number = fileName.split('-')[0];
+
+    return new URL(`../assets/cakes/${number}.png`, import.meta.url).href;
 } 
